d3d2b42d3feca69e6f78f70091dcf9155beed50d
vicaSouzza <vitoriasouzza.vrs@gmail.com>
2021-10-21 17:36:49 -0300

    correcao de calculo de rotas e ajuste nas msg

--- myMethods.py
+++ myMethods.py
@@ -111,7 +111,6 @@ def getPontosDeColetaMaisProximo(pontosDeColeta, latitude, longitude, distMax):
         lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
         # Calcule a distância (em várias unidades) entre dois pontos na Terra usando sua latitude e longitude.
-        # Haversine
         #fonte:  https://www.ti-enxame.com/pt/python/como-posso-estimar-rapidamente-distancia-entre-dois-pontos-latitude-e-longitude/1072488907/
         dlon = lon2 - lon1
         dlat = lat2 - lat1
@@ -152,7 +151,7 @@ def exibeInfoByopcao(numeroinfo):
         print('Exibindo as opções de informações sobre o material escolhido...\n')
 
         for info in informacoes:
-            print("""   * """ + '[{id}] - {nome}'.format(id = info['id'], nome = info['descricao']))
+            print('[{id}] - {nome}'.format(id = info['id'], nome = info['descricao']))
 
         print('\nSe quiser voltar para o menu inicial, digite a palavra VOLTAR ;)')
        
@@ -213,13 +212,13 @@ def exibeInfoByopcao(numeroinfo):
                     limpaConsole()
 
                     if pontosDeColetaMaisProximos[0]['distancia'] <= distMax:
-                        print('Esses são os pontos correspondentes com sua busca: \n')
+                        print('Os pontos mais próximos são esses: \n')
                         
                         for ponto in pontosDeColetaMaisProximos :
-                            print('Distância: ', format(ponto['distancia'], '.2f'), ' Km\nNome: ', ponto['nome'], '\nEndereço: ', ponto['endereco'], '\n')
+                            print('Distância: ', format(ponto['distancia'], '.2f'), ' Km\nNome: ', ponto['nome'], '\nEndereco: ', ponto['endereco'], '\n')
                     else:
-                        print(f'Que pena!! não encontramos nenhum ponto de coleta no raio de {distMax:.2f} km. O local mais próximo encontra-se logo abaixo: ')
-                        print('\nDistância: ', format(pontosDeColetaMaisProximos[0]['distancia'], '.2f'), ' Km\nNome: ', pontosDeColetaMaisProximos[0]['nome'], '\nEndereço: ', pontosDeColetaMaisProximos[0]['endereco'], '\n')
+                        print(f'Que pena!! não foi encontrado nenhum ponto de coleta no raio de {distMax:.2f} km. O local mais próximo encontra-se logo abaixo: ')
+                        print('\nDistância: ', format(pontosDeColetaMaisProximos[0]['distancia'], '.2f'), ' Km\nNome: ', pontosDeColetaMaisProximos[0]['nome'], '\nEndereco: ', pontosDeColetaMaisProximos[0]['endereco'], '\n')
 
                     input('\nPressione qualquer tecla para voltar ao menu inicial!\n')
                 except:
@@ -244,7 +243,7 @@ def exibeInfo(info):
 
 ## Solicita ao usuário a distância máxima que ele está disposto a percorrer
 def digiteDistancia():
-    return input('Qual a distância máxima (em KM) você está disposto a percorrer pelo ponto: ')
+    return input('Qual a distância máxima em KM você deseja percorrer até ponto de coleta: ')
 
 ## Exibe uma mensagem de erro 
 def exibeMensagemErro(msg):
@@ -267,17 +266,15 @@ def getInfo():
             '\n\n\nCaracteristica: Extremamente fáceis de moldar, podem tomar as mais diversas formas. ' +
             'O plástico pode ser rígido ou flexível, transparente ou opaco, resistente à humidade ou ' +
             'solúvel em água - as possibilidades são praticamente infinitas.', 
-
-            'tipo': 'plastico', },
-
-        { 'id': 2, 'descricao': 'Objetos para reutilizar', 
-
-            'obs': 'Aqui vai objetos para reutilizar', 
-
             'tipo': 'plastico' },
 
-        { 'id': 3, 'descricao': 'Forma de utilizar', 
-            'obs': 'Aqui vai a Forma de utilizar', 
+        { 'id': 2, 'descricao': 'Objetos para reutilizar', 
+            'obs': 
+            '\n\n\n------------------- Aqui estão alguns dos objetos que podem ser reutilizados:',
+            'tipo': 'plastico' },
+
+        { 'id': 3, 'descricao': 'Forma de reutilizar', 
+            'obs': 'Aqui vai a Forma de reutilizar', 
             'tipo': 'plastico' },
 
         { 'id': 4, 'descricao': 'Composiçao', 
@@ -289,10 +286,11 @@ def getInfo():
             'tipo': 'metal' },
 
         { 'id': 2, 'descricao': 'Objetos para reutilizar', 
-            'obs': '', 
+            'obs': ''
+            '\n\n\n------------------- Aqui estão alguns dos objetos que podem ser reutilizados:',
             'tipo': 'metal' },
 
-        { 'id': 3, 'descricao': 'Forma de utilizar', 
+        { 'id': 3, 'descricao': 'Forma de reutilizar', 
             'obs': 'metal', 
             'tipo': 'metal' },
 
@@ -305,10 +303,11 @@ def getInfo():
             'tipo': 'vidro' },
 
         { 'id': 2, 'descricao': 'Objetos para reutilizar', 
-            'obs': '', 
+            'obs': ''
+            '\n\n\n------------------- Aqui estão alguns dos objetos que podem ser reutilizados:',
             'tipo': 'vidro' },
 
-        { 'id': 3, 'descricao': 'Forma de utilizar', 
+        { 'id': 3, 'descricao': 'Forma de reutilizar', 
             'obs': 'vidro', 
             'tipo': 'vidro' },
 
@@ -321,10 +320,11 @@ def getInfo():
             'tipo': 'papel' },
 
         { 'id': 2, 'descricao': 'Objetos para reutilizar', 
-            'obs': '', 
+            'obs': 
+            '\n\n\n------------------- Aqui estão alguns dos objetos que podem ser reutilizados:', 
             'tipo': 'papel' },
 
-        { 'id': 3, 'descricao': 'Forma de utilizar', 
+        { 'id': 3, 'descricao': 'Forma de reutilizar', 
             'obs': 'papel', 
             'tipo': 'papel' },
 
@@ -337,15 +337,21 @@ def getInfo():
             'tipo': 'eletronico' },
 
         { 'id': 2, 'descricao': 'Objetos para reutilizar', 
-            'obs': '\n\nMonitores de computador, telefones celulares e baterias, computadores, televisores, câmeras fotográficas, impressoras, radios e aparelho de som, entre outros equipamentos eletrônicos comuns.', 
+            'obs':
+            '\n\n\n------------------- Aqui estão alguns dos objetos que podem ser reutilizados:' 
+            '\n\n\nMonitores de computador, telefones celulares e baterias, computadores, carregadores, fones de ouvido, ' +  
+            'televisores, câmeras fotográficas, impressoras, tablets, mouses, teclados, radios e aparelho de som,' +
+            'eletrodomésticos de grande porte como, geladeira, ' +
+            'fogão, maquina de lavar roupa e louça etc.',
             'tipo': 'eletronico' },
 
-        { 'id': 3, 'descricao': 'Forma de utilizar', 
+        { 'id': 3, 'descricao': 'Forma de reutilizar', 
             'obs': '', 
             'tipo': 'eletronico' },
 
         { 'id': 4, 'descricao': 'Composiçao', 
-            'obs': '\n\nO lixo eletrônico é produzido por materiais de origem inorgânica, por exemplo: cobre, alumínio, metais pesados (mercúrio, cádmio, berílio e chumbo)', 
+            'obs': '\n\nO lixo eletrônico é produzido por materiais de origem inorgânica, como por exemplo:' + 
+            'cobre, alumínio, metais pesados (mercúrio, cádmio, berílio e chumbo)', 
             'tipo': 'eletronico' },
 
     ]
@@ -353,20 +359,20 @@ def getInfo():
 ## Retorna um array/vetor contendo os pontos de coleta disponíveis em Campinas/SP
 def getPontosDeColeta():
     return [
-        { 'id': 1, 'nome': 'Ecoponto - Parque Itajaí', 'endereço': 'R. Celso Soares Couto, S/N - Conj. Hab. Parque Itajaí, Campinas - SP', 'latitude': -22.96143, 'longitude': -47.18951, 'categorias': ['plastico', 'papel', 'vidro', 'metal', 'eletronico'] },
+        { 'id': 1, 'nome': 'Ecoponto - Parque Itajaí', 'endereco': 'R. Celso Soares Couto, S/N - Conj. Hab. Parque Itajaí, Campinas - SP', 'latitude': -22.96143, 'longitude': -47.18951, 'categorias': ['plastico', 'papel', 'vidro', 'metal'] },
         { 'id': 2, 'nome': 'Ponto Verde - Vila Costa e Silva', 'endereco': 'R. Saldanha da Gama, 77 - Vila Costa e Silva, Campinas - SP, 13081-000', 'latitude': -22.856155, 'longitude': -47.068549, 'categorias': ['plastico', 'papel', 'vidro', 'metal', 'eletronico'] },
         { 'id': 3, 'nome': 'Ecoponto - Jardim São Gabriel', 'endereco': 'R. José Martins Lourenço, 140-284 - Jardim São Gabriel, Campinas - SP, 13054-310', 'latitude': -22.94198, 'longitude': -47.02982, 'categorias': ['papel', 'metal', 'vidro', 'plastico'] },
         { 'id': 4, 'nome': 'Ecoponto - Jardim Pacaembu', 'endereco': 'R. Dante Suriani, 2-382 - Chácara Cneo, Campinas - SP, 13033-160', 'latitude': -22.904529, 'longitude': -47.105434, 'categorias': ['plastico', 'papel', 'vidro', 'metal'] },
-        { 'id': 5, 'nome': 'Ecoponto - Jardim Paranapanema', 'endereço': 'R. Serra dÁgua, 326 - Jardim São Fernando, Campinas - SP, 13100-370', 'latitude': -22.91570, 'longitude': -47.03747, 'categorias': ['plastico', 'papel', 'vidro', 'metal']},
-        { 'id': 6, 'nome': 'Ecoponto -  Vila Campos Sales', 'endereço': 'Av. São José dos Campos, S/N - Vila Campos Sales, Campinas - SP, 13040-565', 'latitude': -22.94837, 'longitude': -47.05779, 'categorias': ['papel', 'plastico', 'vidro', 'metal', 'eletronico']},
-        { 'id': 7, 'nome': 'Ecoponto - Parque São Jorge', 'endereço': 'R. Plácida Pretini, 196-270 - Parque São Jorge, Campinas - SP, 13064-812', 'latitude': -22.89579, 'longitude': -47.15631, 'categorias': ['papel', 'plastico', 'vidro', 'metal']},
+        { 'id': 5, 'nome': 'Ecoponto - Jardim Paranapanema', 'endereco': 'R. Serra dÁgua, 326 - Jardim São Fernando, Campinas - SP, 13100-370', 'latitude': -22.91570, 'longitude': -47.03747, 'categorias': ['plastico', 'papel', 'vidro', 'metal']},
+        { 'id': 6, 'nome': 'Ecoponto -  Vila Campos Sales', 'endereco': 'Av. São José dos Campos, S/N - Vila Campos Sales, Campinas - SP, 13040-565', 'latitude': -22.94837, 'longitude': -47.05779, 'categorias': ['papel', 'plastico', 'vidro', 'metal', 'eletronico']},
+        { 'id': 7, 'nome': 'Ecoponto - Parque São Jorge', 'endereco': 'R. Plácida Pretini, 196-270 - Parque São Jorge, Campinas - SP, 13064-812', 'latitude': -22.89579, 'longitude': -47.15631, 'categorias': ['papel', 'plastico', 'vidro', 'metal']},
         { 'id': 8, 'nome': 'HT Papéis Barão - Coleta e reciclagem de resíduos', 'endereco': 'Av. Ruy Rodrigues, 394 - Jardim Novo Campos Eliseos, Campinas - SP, 13060-192', 'latitude': -22.934023, 'longitude': -47.105661, 'categorias': ['plastico', 'papel'] },
         { 'id': 9, 'nome': 'Ecoponto -  Vila União', 'endereco': 'R. Manoel Gomes Ferreira, 42 - Parque Tropical, Campinas - SP, 13060-523', 'latitude': -22.936016, 'longitude': -47.118061, 'categorias': ['papel', 'plastico', 'vidro', 'metal'] },
         { 'id': 10, 'nome': 'Ecoponto / Ponto Verde', 'endereco': 'Av. Santa Isabel, 2300 - Barão Geraldo, Campinas - SP, 13084-012', 'latitude': -22.817244, 'longitude': -47.100531, 'categorias': ['papel', 'plastico', 'vidro', 'metal'] },
         { 'id': 11, 'nome': 'GMV Recycle', 'endereco': 'Rod. Lix da Cunha, 911 - Jardim Nova America, Campinas - SP, 13070-715', 'latitude': -22.898166, 'longitude': -47.093476, 'categorias': ['papel', 'eletronico'] },
         { 'id': 12, 'nome': 'Ecoponto -  Jardim Eulina',  'endereco': 'Av. Mal. Rondon, 2296-2382 - Jardim Chapadão, Campinas - SP', 'latitude': -22.891751, 'longitude': -47.100940, 'categorias': ['papel', 'vidro', 'plastico', 'metal'] },
         { 'id': 13, 'nome': 'Reversis - Reciclagem de Eletrônicos e Informática',  'endereco': 'R. da Abolição, 1900 - Pte. Preta, Campinas - SP, 13041-445', 'latitude': -22.926823, 'longitude': -47.042984, 'categorias': ['eletronico'] },
-        { 'id': 14, 'nome': 'Ponto Verde - Carlos Grimaldi', 'endereço': 'R. Cōnego Pedro Bonhomme, 2424 - Jardim Bela Vista, Campinas - SP, 13077-003', 'latitude': -22.87611, 'longitude': -47.03918, 'categorias': ['papel', 'vidro', 'plastico', 'metal']},
+        { 'id': 14, 'nome': 'Ponto Verde - Carlos Grimaldi', 'endereco': 'R. Cōnego Pedro Bonhomme, 2424 - Jardim Bela Vista, Campinas - SP, 13077-003', 'latitude': -22.87611, 'longitude': -47.03918, 'categorias': ['papel', 'vidro', 'plastico', 'metal']},
         { 'id': 15, 'nome': 'Cooperativa de Recicláveis Santa Genebra',  'endereco': 'R. Estácio de Sá, 577 - Jardim Santa Genebra, Campinas - SP, 13084-751', 'latitude': -22.852778, 'longitude': -47.074819, 'categorias': ['papel', 'vidro', 'plastico', 'metal'] }
     ]
 
